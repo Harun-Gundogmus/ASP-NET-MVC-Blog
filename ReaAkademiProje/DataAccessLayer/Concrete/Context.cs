613b496a891a2c9f6d60ab2f660d685eae1e787b
Harun-Gundogmus <gundogmusharun2@gmail.com>
2022-11-16 23:49:45 +0300

    Final

---
+++ ReaAkademiProje/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-5ETPORJT;database=ReaDB;integrated security=true;");
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
